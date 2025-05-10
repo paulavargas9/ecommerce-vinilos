736599834f7a000d95bb6e01338da81d669bd2ac
paulavargas9 <143019095+paulavargas9@users.noreply.github.com>
2025-05-10 19:56:30 +0200

    Paso 3.2: Configuradas rutas en App.jsx y creadas páginas Shop, Contact, About y NotFound

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,14 +4,21 @@ import Footer from "./components/Footer";
 
 // Páginas
 import Home from "./pages/Home";
+import Shop from "./pages/Shop";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
 
 export default function App() {
   return (
     <Router>
       <NavBar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        {/* Aquí agregaremos más rutas luego */}
+      <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
       <Footer />
     </Router>
