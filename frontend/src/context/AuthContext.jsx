58584df582ae675fc07d4ee8732af691517c751b
paulavargas9 <143019095+paulavargas9@users.noreply.github.com>
2025-05-17 12:03:30 +0200

    Añadido AuthContext con login simulado y gestión de sesión

---
+++ frontend/src/context/AuthContext.jsx
@@ -0,0 +1,41 @@
+// src/context/AuthContext.jsx
+import { createContext, useContext, useState, useEffect } from "react";
+
+const AuthContext = createContext();
+
+export const useAuth = () => useContext(AuthContext);
+
+export function AuthProvider({ children }) {
+  const [user, setUser] = useState(() => {
+    const savedUser = localStorage.getItem("user");
+    return savedUser ? JSON.parse(savedUser) : null;
+  });
+
+  useEffect(() => {
+    if (user) {
+      localStorage.setItem("user", JSON.stringify(user));
+    } else {
+      localStorage.removeItem("user");
+    }
+  }, [user]);
+
+  const login = (email, password) => {
+    //  la petición real al backend
+    if (email === "admin@demo.com" && password === "1234") {
+      setUser({ email });
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  const logout = () => {
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
